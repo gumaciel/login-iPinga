1ef14dd566a77fcd0f5bf87163a99432630c7e6b
gustavopersyos <gustavo.maciel@persyos.com.br>
2018-11-30 00:14:18 -0200

    atualização 30/11/2018

---
+++ testa_login.php
@@ -0,0 +1,7 @@
+<form action="logar.php" method="POST">
+
+	Email: <input type="text" name="email_app"> <br />
+	Senha: <input type="text" name="senha_app"> <br />
+	
+	<input type="submit" value="Logar">
+</form>
